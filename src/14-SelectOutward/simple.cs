0676d6fe22843d33c198a5d554b17dc154c5988e
Walt Ritscher <waltritscher@outlook.com>
2024-08-08 00:00:47 -0700

    remove extra folder

--- src/14-SelectOutward/simple.cs
+++ src/14-SelectOutward/simple.cs
@@ -3,6 +3,7 @@ public void Demo ()
  var total =0;
 for (int i = 0; i < 6; i++)
 {
+	var word = "hello";
 	total  += i;
 }
 }
--- src/15-SelectOutward/simple.cs
+++
@@ -1,9 +0,0 @@
-public void Demo ()
-{
- var total =0;
-for (int i = 0; i < 6; i++)
-{
-	var word = "hello";
-	total  += i;
-}
-}
